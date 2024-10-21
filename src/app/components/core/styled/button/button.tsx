d4ca855d6800abdfb6dd259f4d3f43dc8ec76f37
Danny Vasta <djv711@gmail.com>
2024-10-21 14:19:28 -0400

    Add Button icon variant, theme toggle sun and moon svg

--- src/app/components/core/styled/button/button.tsx
+++ src/app/components/core/styled/button/button.tsx
@@ -2,7 +2,7 @@ import { ComponentPropsWithoutRef } from "react";
 import styled, { css } from "styled-components";
 import { disabled, smallFont, fontWeightSemiBold } from "../styles";
 
-type ButtonVariants = "primary" | "secondary" | "destructive" | undefined;
+type ButtonVariants = "primary" | "secondary" | "destructive" | "icon" | undefined;
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
   $variant?: ButtonVariants;
@@ -11,6 +11,7 @@ interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
 const VARIANTS = {
   primary: css`
     background: #9147ff;
+    color: #efeff1;
     &:hover:enabled {
       background: #772ce8;
     }
@@ -27,11 +28,12 @@ const VARIANTS = {
       background: #ff4f4d;
     }
   `,
+  icon: css`
+    background: none;
+  `
 };
 
 const Button = styled.button<{ $variant?: ButtonVariants }>`
-  color: #efeff1;
-
   ${smallFont}
   ${fontWeightSemiBold}
 
--- src/app/components/theme-toggle.tsx
+++ src/app/components/theme-toggle.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { Button } from "../components/core/styled/button";
+import Moon from "./icons/moon";
+import Sun from "./icons/sun";
 import { useTheme } from "../theme-provider";
 
 const ThemeToggle = () => {
-  const { toggleTheme } = useTheme();
+  const {
+    theme,
+    toggleTheme,
+    colorScheme: { color },
+  } = useTheme();
   return (
-    <Button className="w-min" onClick={toggleTheme}>
-      toggle theme
+    <Button $variant="icon" className={`w-min ${color}`} onClick={toggleTheme}>
+      {theme === "dark" ? <Moon /> : <Sun />}
     </Button>
   );
 };
